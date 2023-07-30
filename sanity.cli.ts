012a8202d038a614cdd50b8993e173b97eb69a11
Jirayut Leeupathumvong <>
2023-07-30 16:59:14 +0700

    create project

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -7,7 +7,11 @@ loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
 // @TODO report top-level await bug
 // Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
 // const { projectId, dataset } = await import('lib/sanity.api')
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+const projectId = "n7kx2r8o"
+const dataset = "production"
 
 export default defineCliConfig({ api: { projectId, dataset } })
